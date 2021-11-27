76623a8b4a217db0a2cd9694cb0fb193d0a82e56
Cory Thompson <cory.thompson98@gmail.com>
2021-11-27 09:43:32 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,13 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericalChar = "0123456789";
+var specialChar = "!@#$%^&*()/*-<=>;:[]{}";
+
 // Write password to the #password input
-function writePassword() {
+function generatePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
