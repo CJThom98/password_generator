8141929260dfe61eab2fd48717ecff10b6d5c576
Cory Thompson <cory.thompson98@gmail.com>
2021-11-27 10:42:20 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -4,20 +4,18 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numericalChar = "0123456789";
-var specialChar = "!@#$%^&*()/*-<=>;:[]{}";
-
 // Write password to the #password input
 function generatePassword() {
 
+  var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericalChar = "0123456789";
+  var specialChar = "!@#$%^&*()/*-<=>;:[]{}";
   var password = "";
   var passwordChar = "";
 
   // Length of password prompt
   var passwordLengthUser = prompt("What is the length of your desired password? Must be between 8 and 128 characters.");
-  passwordLengthUser = parseInt(passwordLengthUser);
 
   if (passwordLengthUser < 8) {
     alert("Password must be more than 7 characters!");
